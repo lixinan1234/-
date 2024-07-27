c865f9b19d2f523dab7bf855b4a7a8815637ff4d
lixinan1234 <2489460735@qq.com>
2024-07-27 17:08:02 +0800

    菜品分页查询业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,10 +2,15 @@ package com.sky.service;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 public interface DishService {
 
     //新增菜品和对应的口味
     public void saveWithFlavor(DishDTO dishDTO);
+
+    //菜品分页查询
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
