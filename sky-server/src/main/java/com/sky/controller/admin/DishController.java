3ee771e908b7c4b1db3c82b75531a0f56fecc3a8
lixinan1234 <2489460735@qq.com>
2024-08-01 10:04:03 +0800

    修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -5,6 +5,7 @@ import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -52,6 +53,24 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("批量删除的是：{}",ids);
         dishService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    //根据id查询菜品
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询菜品")
+    public Result<DishVO> getByID(@PathVariable Long id){
+        log.info("根据id查询的数据：{}",id);
+        DishVO dishVO =  dishService.getByIdWithFlavor(id);
+        return Result.success(dishVO);
+    }
+
+    //修改菜品
+    @PutMapping
+    @ApiOperation("修改菜品")
+    public Result update(@RequestBody DishDTO dishDTO){
+        log.info("修改的菜品是：{}",dishDTO);
+        dishService.updateWithFlavor(dishDTO);
         return Result.success();
     }
 
--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 import com.sky.entity.DishFlavor;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -16,4 +17,14 @@ public interface DishFlavorMapper {
     //根据菜品Id来删除口味数据
     @Delete("delete from dish_flavor where dish_id = #{dishId}")
     void deleteByDishId(Long dishId);
+
+    //根据菜品id集合批量删除口味数据
+    void deleteByDishIds(List<Long> dishIds);
+
+    //跟据id查询菜品口味数据
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
+
+
+
 }
